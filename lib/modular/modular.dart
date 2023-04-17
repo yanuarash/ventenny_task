afee7381b8d9f7bfdb2bcd567332ea6269deeebc
yanuarash <yanuar.ash@gmail.com>
2023-04-17 13:57:10 +0700

    add modular

---
+++ lib/modular/modular.dart
@@ -0,0 +1,13 @@
+import 'package:ventenny_task/cubit/itunes_cubit.dart';
+import 'package:ventenny_task/networking/api/itunes_rest_client.dart';
+import 'package:ventenny_task/networking/dio/dio_client.dart';
+import 'package:ventenny_task/networking/repo/itunes_repo.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind<ItunesRepo>(((i) => ItunesRepo(ItunesRestClient(dioClient())))),
+        Bind<ItunesCubit>(((i) => ItunesCubit())),
+      ];
+}
