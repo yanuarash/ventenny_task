32c3572f20c78a872a7abe823f69891dda5e241e
yanuarash <yanuar.ash@gmail.com>
2023-04-18 02:34:13 +0700

    add video player on homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,11 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ventenny_task/cubit/itunes_cubit.dart';
 import 'package:ventenny_task/widgets/list_itunes_widget.dart';
 import 'package:ventenny_task/widgets/search_widget.dart';
+import 'package:video_player/video_player.dart';
 
 class HomePageWidget extends StatefulWidget {
   const HomePageWidget({super.key});
@@ -13,15 +15,83 @@ class HomePageWidget extends StatefulWidget {
 }
 
 class _HomePageWidgetState extends State<HomePageWidget> {
+  final bloc = Modular.get<ItunesCubit>();
+  final searchController = TextEditingController();
+
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _chewieController = ChewieController(
+      videoPlayerController: VideoPlayerController.network(
+          'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'),
+      aspectRatio: 4 / 3,
+      autoInitialize: false,
+      showControls: false,
+      showOptions: false,
+      showControlsOnInitialize: false,
+      looping: true,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _initializePlay(String url) {
+    _chewieController.videoPlayerController.dispose();
+    _chewieController.dispose();
+
+    setState(() {});
+
+    _chewieController = ChewieController(
+      videoPlayerController: VideoPlayerController.network(url),
+      aspectRatio: 16.0 / 9.0,
+      autoInitialize: true,
+      autoPlay: true,
+      looping: true,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _chewieController.videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final bloc = Modular.get<ItunesCubit>();
-    final searchController = TextEditingController();
-
     return Column(
       children: [
         Flexible(
-          flex: 2,
+          flex: 1,
+          child: Container(
+            color: Colors.black,
+            child: Chewie(controller: _chewieController),
+          ),
+        ),
+        Flexible(
+          flex: 1,
           child: SizedBox.expand(
             child: BlocConsumer<ItunesCubit, ItunesState>(
               bloc: bloc,
@@ -37,7 +107,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               builder: (context, state) {
                 return state.maybeWhen(
                   success: (data) {
-                    return ListItunesWidget(data: data);
+                    return ListItunesWidget(
+                      data: data,
+                      onTap: _initializePlay,
+                    );
                   },
                   loading: () {
                     return const CircularProgressIndicator();
