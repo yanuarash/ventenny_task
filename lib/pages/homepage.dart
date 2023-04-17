62c69e5c1a27a331c104d3dcb558c29f1476a3fd
yanuarash <yanuar.ash@gmail.com>
2023-04-17 20:39:15 +0700

    change homapage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:ventenny_task/cubit/itunes_cubit.dart';
+import 'package:ventenny_task/widgets/list_itunes_widget.dart';
+import 'package:ventenny_task/widgets/search_widget.dart';
 
 class HomePageWidget extends StatefulWidget {
   const HomePageWidget({super.key});
@@ -12,8 +15,49 @@ class HomePageWidget extends StatefulWidget {
 class _HomePageWidgetState extends State<HomePageWidget> {
   @override
   Widget build(BuildContext context) {
+    final bloc = Modular.get<ItunesCubit>();
+    final searchController = TextEditingController();
+
     return Column(
-      children: [],
+      children: [
+        Flexible(
+          flex: 2,
+          child: SizedBox.expand(
+            child: BlocConsumer<ItunesCubit, ItunesState>(
+              bloc: bloc,
+              listener: (context, state) {
+                state.whenOrNull(
+                  error: (error) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(error),
+                    ));
+                  },
+                );
+              },
+              builder: (context, state) {
+                return state.maybeWhen(
+                  success: (data) {
+                    return ListItunesWidget(data: data);
+                  },
+                  loading: () {
+                    return const CircularProgressIndicator();
+                  },
+                  orElse: () {
+                    return const SizedBox();
+                  },
+                );
+              },
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 0,
+          child: BlocProvider.value(
+            value: bloc,
+            child: SearchWidget(controller: searchController),
+          ),
+        )
+      ],
     );
   }
 }
