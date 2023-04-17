127ff8027582ab1e1f4f971ebe8d2d8eb0a23a59
yanuarash <yanuar.ash@gmail.com>
2023-04-17 13:52:20 +0700

    add dio config

---
+++ lib/networking/dio/dio_client.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+import 'dio_options.dart';
+
+Dio dioClient() {
+  final Dio dio = Dio();
+
+  dio
+    ..options.contentType = contentType
+    ..options.baseUrl = baseUrl
+    ..options.connectTimeout = const Duration(seconds: connectionTimeout)
+    ..options.receiveTimeout = const Duration(seconds: receiveTimeout);
+
+  return dio;
+}
---
+++ lib/networking/dio/dio_options.dart
@@ -0,0 +1,7 @@
+const String baseUrl = 'https://itunes.apple.com/';
+
+const String contentType = 'application/json';
+
+const int receiveTimeout = 20000;
+
+const int connectionTimeout = 20000;
