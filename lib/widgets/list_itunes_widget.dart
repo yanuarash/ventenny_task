c6e196c3fac438733edc6170e77aee0a3bdc884e
yanuarash <yanuar.ash@gmail.com>
2023-04-18 13:49:45 +0700

    refactor listItunesWidge to statefulWidget

--- lib/widgets/list_itunes_widget.dart
+++ lib/widgets/list_itunes_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ventenny_task/models/item_search.dart';
 import 'package:ventenny_task/widgets/itunes_item_widget.dart';
 
-class ListItunesWidget extends StatelessWidget {
+class ListItunesWidget extends StatefulWidget {
   const ListItunesWidget({
     super.key,
     required this.data,
@@ -13,21 +13,34 @@ class ListItunesWidget extends StatelessWidget {
   final Function(String url) onTap;
 
   @override
+  State<ListItunesWidget> createState() => _ListItunesWidgetState();
+}
+
+class _ListItunesWidgetState extends State<ListItunesWidget> {
+  int indexSelected = 0;
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: data.results!.length,
+      itemCount: widget.data.results!.length,
       shrinkWrap: true,
       itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            if (data.results![index].previewUrl != null) {
-              onTap(data.results![index].previewUrl!);
-            }
-          },
-          child: ItunesItemWidget(
-            artistName: data.results![index].artistName ?? "",
-            albumName: data.results![index].collectionName ?? "",
-            trackName: data.results![index].trackName ?? "",
+        return Container(
+          color: indexSelected == index + 1 ? Colors.grey[200] : Colors.white,
+          child: InkWell(
+            onTap: () {
+              if (widget.data.results![index].previewUrl != null) {
+                widget.onTap(widget.data.results![index].previewUrl!);
+              }
+              setState(() {
+                indexSelected = index + 1;
+              });
+            },
+            child: ItunesItemWidget(
+              artworkUrl: widget.data.results![index].artworkUrl100 ?? "",
+              artistName: widget.data.results![index].artistName ?? "",
+              albumName: widget.data.results![index].collectionName ?? "",
+              trackName: widget.data.results![index].trackName ?? "",
+            ),
           ),
         );
       },
