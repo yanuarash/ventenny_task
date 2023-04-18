6c2b9787fa499dc3e17568fd4243c7f169f39086
yanuarash <yanuar.ash@gmail.com>
2023-04-18 13:51:03 +0700

    refactor itunesItemWidget

--- lib/widgets/itunes_item_widget.dart
+++ lib/widgets/itunes_item_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ItunesItemWidget extends StatelessWidget {
@@ -14,48 +15,39 @@ class ItunesItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            children: [
-              const Icon(Icons.play_circle_fill),
-              const SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    trackName,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                    overflow: TextOverflow.fade,
-                    maxLines: 1,
-                    softWrap: false,
-                  ),
-                  const SizedBox(height: 2),
-                  if (albumName.isNotEmpty) ...[
-                    Text(
-                      albumName,
-                      style: const TextStyle(),
-                    )
-                  ],
-                  const SizedBox(height: 2),
-                  Text(
-                    artistName,
-                    style: const TextStyle(),
-                  ),
-                ],
-              ))
-            ],
-          ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(16, 16, 8, 16),
+          child: Icon(Icons.play_circle_fill),
         ),
-        const Divider(
-          height: 1,
-        )
+        const SizedBox(width: 8),
+        Expanded(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              trackName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              overflow: TextOverflow.fade,
+              maxLines: 1,
+              softWrap: false,
+            ),
+            const SizedBox(height: 2),
+            if (albumName.isNotEmpty) ...[
+              Text(
+                albumName,
+                style: const TextStyle(),
+              )
+            ],
+            const SizedBox(height: 2),
+            Text(
+              artistName,
+              style: const TextStyle(),
+            ),
+          ],
+        ))
       ],
     );
   }
