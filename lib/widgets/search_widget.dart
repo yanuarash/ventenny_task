d7f7d63a85d4cbb9a39ad07f6b733aec06088636
yanuarash <yanuar.ash@gmail.com>
2023-04-18 14:37:58 +0700

    refactor search widget

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -9,29 +9,32 @@ class SearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: TextField(
-        style: const TextStyle(color: Colors.white),
-        onSubmitted: (value) => onSubmitted(value),
-        textInputAction: TextInputAction.search,
-        controller: controller,
-        decoration: const InputDecoration(
-          hintText: 'Search Artist Name - ex: "Jack Johnson"',
-          hintStyle: TextStyle(color: Colors.grey),
-          filled: true,
-          fillColor: Colors.black,
-          prefixIcon: Icon(Icons.search),
-          prefixIconColor: Colors.white,
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(50))),
-          focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(50))),
-          border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(50))),
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          style: const TextStyle(color: Colors.white),
+          onSubmitted: (value) => onSubmitted(value),
+          textInputAction: TextInputAction.search,
+          controller: controller,
+          decoration: const InputDecoration(
+            hintText: 'Search Artist Name - ex: "Jack Johnson"',
+            hintStyle: TextStyle(color: Colors.grey),
+            filled: true,
+            fillColor: Colors.black,
+            prefixIcon: Icon(Icons.search),
+            prefixIconColor: Colors.white,
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(50))),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(50))),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(50))),
+          ),
         ),
       ),
     );
