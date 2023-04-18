06e7769217f524221c37c01bd4eb921e38f41450
yanuarash <yanuar.ash@gmail.com>
2023-04-18 16:42:31 +0700

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:ventenny_task/cubit/itunes_cubit.dart';
 import 'package:ventenny_task/modular/modular.dart';
 import 'package:ventenny_task/pages/homepage.dart';
 
